bd35f1a87f12061bfb305d70236b53f07208f2f5
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-10-02 14:56:56 +0200

    Removed componentWillReceiveProps

--- src/index.tsx
+++ src/index.tsx
@@ -130,14 +130,13 @@ export default class AnimatedDrawer extends React.Component<Props, State> {
         }).start();
     }
 
-    componentWillReceiveProps(nextProps: Props) {
-        if (nextProps.isOpen) {
-            this.open();
-        } else {
-            this.close();
-        }
-    }
-
+    // componentWillReceiveProps(nextProps: Props) {
+    //     if (nextProps.isOpen) {
+    //         this.open();
+    //     } else {
+    //         this.close();
+    //     }
+    // }
 
     componentWillUnmount() {
         this.state.pan.removeAllListeners();
